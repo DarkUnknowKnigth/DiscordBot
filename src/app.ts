944ccd3349f3d30d3e2cac4cc555fa6318c67a77
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2021-05-03 18:27:44 -0500

    bot basic

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
-import {Client} from 'discord.js';
+import env from "dotenv";
+import { Client } from "discord.js";
+class Bot{
+  constructor(private client: Client){
+    env.config();
+    console.log('Bot Initialized');
+    this.loadEvents();
+    this.loadCredentials();
+  }
+  loadCredentials(){
+    this.client.login(process.env.DISCORD_TOKEN);
+  }  
+  loadEvents(){
+    this.client.on('ready', ()=>{
+      console.log(`Logged in as ${client.user.tag}!`);
+    });
+    this.client.on('message', msg => {
+      if (msg.content === 'ping') {
+        msg.reply('Pong!');
+      }
+    });
+  }
+
+}
 const client = new Client();
-client.on('ready',()=>{
-  console.log(`Logged in as ${client.user?.tag}`);
-});
+const bot = new Bot(client);
